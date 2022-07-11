f3ccb780b1967890b4e6ed7a3c0a16ba2f5271c4
Rikuya Chiba <rikuya.chiba@gmail.com>
2022-07-11 23:43:06 +0900

    If start button is pressed, then question text is appeared

---
+++ script.js
@@ -0,0 +1,13 @@
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+    startButton.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+}
+
+function setNextQuestion() {
+
+}
